e16ff0888863017ccfa81e6c7db8e4a5e6a1b550
Charles Johnson <charles_d_johnson@aol.com>
2018-11-04 17:40:03 -0500

    Navigation Bar Tweeked

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,15 @@ $(function(){
 	console.log(pageName);
 	$('#header ul li a[href^="' + pageName + '"]').addClass('selected');
 })
+
+function changeVisible(elementID){
+	console.log("in changeVisible "+elementID);
+	document.getElementById(elementID).classList.toggle("hidden")
+	/*if(document.getElementsByClassName(elementID).classList.contains("hidden") ){
+		document.getElementsByClassName(elementID).className = "show";
+	}else{
+		document.getElementsByClassName(elementID).className = "hidden";
+	}*/
+}
+
+// $("nav li ol").hide();
