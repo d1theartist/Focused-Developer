ec51935743791dbb9633f70e78323e455a48e944
Charles Johnson <charles_d_johnson@aol.com>
2018-12-02 10:43:49 -0500

    created 'createProject' to simulate database of projects

--- src/com/focuseddeveloper/beans/Project.java
+++ src/com/focuseddeveloper/beans/Project.java
@@ -1,20 +1,47 @@
 package com.focuseddeveloper.beans;
 
+import java.util.ArrayList;
+
 public class Project {
 	
+	private String primaryProjectTech;
 	private String title;
 	private String subtitle;
 	private String summary;
 	
-	private String[][] keyFeatures = { {"Feature 1","This is Awesome"},{"Feature 2","Amazing!!!"},{"Feature 3","Wait... there's more!"} };
+	private String urlString;
 	
+	private ArrayList<String> tags;
+	
+	//private String[][] keyFeatures = { {"Feature 1","This is Awesome"},{"Feature 2","Amazing!!!"},{"Feature 3","Wait... there's more!"} };
+	private ArrayList<String[]> keyFeatures = new ArrayList<String[]>(); 
 	
 	
 	public Project() {
+		primaryProjectTech = "C++";
 		title = "Battleship";
 		subtitle = "The classic board game";
 		summary = "This is the game you know and love. The game is single player vs the CPU.";	
 		
+		keyFeatures.add(new String[] {"Feature 1","This is Awesome"});
+		keyFeatures.add(new String[] {"Feature 2","Amazing!!!"});
+		keyFeatures.add(new String[] {"Feature 3","Wait... there's even more!"});
+		
+		urlString = "https://github.com/";
+		tags.add("Language 1");
+		tags.add("tools 1");
+		tags.add("tools 2");
+	}
+	
+	public Project(String primaryProjectTech, String title, String subtitle, String summary, ArrayList<String[]> keyFeatures, String urlString, ArrayList<String> tags) {
+		this.primaryProjectTech = primaryProjectTech;
+		this.title = title;
+		this.subtitle = subtitle;
+		this.summary = summary;
+		
+		this.keyFeatures = keyFeatures;
+		this.urlString = urlString;
+		this.tags = tags;
 	}
 
 
@@ -43,13 +70,45 @@ public class Project {
 	}
 
 
-	public String[][] getKeyFeatures() {
+	public ArrayList<String[]> getKeyFeatures() {
 		return keyFeatures;
 	}
 
 
-	public void setKeyFeatures(String[][] keyFeatures) {
+	public void setKeyFeatures(ArrayList<String[]> keyFeatures) {
 		this.keyFeatures = keyFeatures;
+	}
+	
+	public void addKeyFeature(String[] feature) {
+		keyFeatures.add(feature);
+	}
+
+	public String getPrimaryProjectTech() {
+		return primaryProjectTech;
+	}
+
+	public void setPrimaryProjectTech(String primaryProjectTech) {
+		this.primaryProjectTech = primaryProjectTech;
+	}
+
+	public String getUrlString() {
+		return urlString;
+	}
+
+	public void setUrlString(String urlString) {
+		this.urlString = urlString;
+	}
+
+	public ArrayList<String> getTags() {
+		return tags;
+	}
+
+	public void setTags(ArrayList<String> tags) {
+		this.tags = tags;
+	}
+	
+	public void addTag(String tag) {
+		tags.add(tag);
 	}
 
 }
---
+++ src/com/focuseddeveloper/servedata/createProjects.java
@@ -0,0 +1,48 @@
+package com.focuseddeveloper.servedata;
+
+import java.util.ArrayList;
+
+import com.focuseddeveloper.beans.Project;
+import com.focuseddeveloper.beans.ProjectTech;
+
+public class createProjects {
+	
+	static private ArrayList<String[]> keyFeaturesExample = new ArrayList<String[]>(); 
+	
+	static private String c = "C=+";
+	static private String java = "Java";
+	static private String android = "Android";
+	
+	static private String subTitleExample = "subtitles are cool!";
+	
+	static private String summaryExample = "This is a summary of what the project.  I include the basic functionality of the project, and discuss what"
+			+ " I found interesting and challenging about the project.";
+
+	static private ArrayList<Project> techList = new ArrayList<Project>();
+	
+	static private ArrayList<String> tagsExample = new ArrayList<String>();
+	
+	private String urlStringExample = "https://github.com/"; 
+		
+	
+	{
+		keyFeaturesExample.add(new String[] {"Feature 1","This is Awesome"});
+		keyFeaturesExample.add(new String[] {"Feature 2","Amazing!!!"});
+		keyFeaturesExample.add(new String[] {"Feature 3","Wait... there's even more!"});
+		
+		tagsExample.add("Language 1");
+		tagsExample.add("tools 1");
+		tagsExample.add("tools 2");
+		
+		techList.add( new Project(c,"Battleship",subTitleExample,summaryExample, keyFeaturesExample, urlStringExample, tagsExample) );
+		techList.add( new Project(c,"Video Store",subTitleExample,summaryExample, keyFeaturesExample, urlStringExample, tagsExample) );
+		techList.add( new Project(java,"Focused Developer Site",subTitleExample,summaryExample, keyFeaturesExample, urlStringExample, tagsExample) );
+		techList.add( new Project(java,"Business Transactions",subTitleExample,summaryExample, keyFeaturesExample, urlStringExample, tagsExample) );
+		techList.add( new Project(android,"Abstract News",subTitleExample,summaryExample, keyFeaturesExample, urlStringExample, tagsExample) );
+		techList.add( new Project(android,"Popular Movies",subTitleExample,summaryExample, keyFeaturesExample, urlStringExample, tagsExample) );
+		techList.add( new Project(android,"Build it Bigger",subTitleExample,summaryExample, keyFeaturesExample, urlStringExample, tagsExample) );
+		
+	}
+	
+
+}
